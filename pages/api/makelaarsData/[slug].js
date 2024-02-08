5806c61ae7deaaac6ede458dd484a6314e77f190
larsdeklijne <larsdeklijne@gmail.com>
2024-02-08 19:21:36 +0100

    made dynamic route /api/makelaarsData/[slug]

--- pages/api/makelaarsData/[slug].js
+++ pages/api/makelaarsData/[slug].js
@@ -1,17 +1,19 @@
 // api/makelaarsData.js
-import { getMakelaarInfo } from '../../lib/data';
+import { getMakelaarInfo } from '../../../lib/data';
 
 export default async function handler(req, res) {
   try {
-    const makelaar = req.query.makelaar;
+    const { slug } = req.query;
+
+    const makelaar = slug;
 
     if (!makelaar) {
-      return res.status(400).json({ error: 'Missing makelaar parameter' });
+      return res.status(400).json({ error: 'Missing makelaar slug /makelaarsData/makelaar1' });
     }
 
     const name = await getMakelaarInfo(makelaar);
     
-    res.status(200).json({ name });
+    return res.status(200).json({ name });
   } catch (error) {
     console.error('API Route Error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
--- pages/api/websiteScraper.js
+++ pages/api/websiteScraper.js
@@ -2,31 +2,33 @@ import puppeteer from 'puppeteer';
 
 export default async function handler(req, res) {
   try {
-    // Call /api/makelaarsData?makelaar=makelaar6
-    const apiResponse = await fetch('/api/makelaarsData?makelaar=makelaar6');
+
+    const apiResponse = await fetch(process.env.URL + '/api/makelaarsData/makelaar6');
     const makelaarData = await apiResponse.json();
 
-    console.log(makelaarData);
+    console.log(makelaarData)
 
-    // Extract URL from the response
-    const { url } = makelaarData.url;
+    return res.status(200).json({ makelaarData });
 
-    // Use Puppeteer to scrape the website and capture a screenshot
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+    // // Extract URL from the response
+    // const { url } = makelaarData.url;
 
-    // Capture a screenshot
-    const screenshotPath = '/screenshots/screenshot.png';
-    await page.screenshot({ path: screenshotPath });
+    // // Use Puppeteer to scrape the website and capture a screenshot
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
+    // await page.goto(url);
 
-    // Close the browser
-    await browser.close();
+    // // Capture a screenshot
+    // const screenshotPath = '/screenshots/screenshot.png';
+    // await page.screenshot({ path: screenshotPath });
 
-    // Respond with the screenshot file
-    res.status(200).sendFile(screenshotPath, { root: '.' });
+    // // Close the browser
+    // await browser.close();
+
+    // // Respond with the screenshot file
+    // res.status(200).sendFile(screenshotPath, { root: '.' });
   } catch (error) {
     console.error('Scraping Error:', error);
-    res.status(500).json({ error: 'Failed to capture screenshot' });
+    res.status(500).json({ error: 'Er is wat fout gegaan' });
   }
 }
